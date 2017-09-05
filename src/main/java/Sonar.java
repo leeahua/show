789b0289c56e77e6e52095d49b9e3a414037c01f
liyaohua <liyaohua@hexindai.com>
2017-09-05 17:12:03 +0800

    commmit

---
+++ src/main/java/Sonar.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2017/9/5.
+ */
+public class Sonar {
+
+    public static void main(String args[]){
+        int target = -5;
+        int num = 3;
+
+        target =- num;
+        target =+ num;
+
+        System.out.println("hello sonar ,this is an error");
+    }
+}
